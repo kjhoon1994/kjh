39fdce982117f72db94305a5396d71573e867a23
kjhoon1994 <47774697+kjhoon1994@users.noreply.github.com>
2019-04-08 10:19:28 +0900

    aa

---
+++ 03.JDBC/20190408_jdbc/src/jdbc001/rlawogns.sql
@@ -0,0 +1,12 @@
+-- id_, age, first_, last_
+CREATE TABLE employees(
+   id_ NUMBER
+   ,age NUMBER
+   ,first_ VARCHAR(20)
+   ,last_ VARCHAR(20)
+);
+INSERT INTO Employees(id_, age, first_, last_) VALUES (100, 18, 'Zara', 'Ali');
+INSERT INTO Employees(id_, age, first_, last_) VALUES (101, 25, 'Mahnaz', 'Fatma');
+INSERT INTO Employees(id_, age, first_, last_) VALUES (102, 30, 'Zaid', 'Khan');
+INSERT INTO Employees(id_, age, first_, last_) VALUES (103, 28, 'Sumit', 'Mittal');
+COMMIT;
