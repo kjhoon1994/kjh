e57d1310be978fc7709d8ec21c8fa10e8f982014
kjhoon1994 <47774697+kjhoon1994@users.noreply.github.com>
2019-04-08 11:20:49 +0900

    CRUD -SELECT

---
+++ 03.JDBC/20190408_jdbc/src/jdbc002/Main.java
@@ -0,0 +1,76 @@
+package jdbc002;
+//1 단계 : Import required packages	
+import java.sql.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+	/*	
+	  
+	Common JDBC Components
+		The JDBC API provides the following interfaces and classes −
+		DriverManager: This class manages a list of database drivers. Matches connection requests from the java application with the proper database driver using communication sub protocol. The first driver that recognizes a certain subprotocol under JDBC will be used to establish a database Connection.
+		Driver: This interface handles the communications with the database server. You will interact directly with Driver objects very rarely. Instead, you use DriverManager objects, which manages objects of this type. It also abstracts the details associated with working with Driver objects.
+		Connection: This interface with all methods for contacting a database. The connection object represents communication context, i.e., all communication with database is through connection object only.
+		Statement: You use objects created from this interface to submit the SQL statements to the database. Some derived interfaces accept parameters in addition to executing stored procedures.
+		ResultSet: These objects hold data retrieved from a database after you execute an SQL query using Statement objects. It acts as an iterator to allow you to move through its data.
+		SQLException: This class handles any errors that occur in a database application.
+	
+	Creating JDBC Application
+		There are following six steps involved in building a JDBC application −
+		Import the packages: Requires that you include the packages containing the JDBC classes needed for database programming. Most often, using import java.sql.* will suffice.
+		Register the JDBC driver: Requires that you initialize a driver so you can open a communication channel with the database.
+		Open a connection: Requires using the DriverManager.getConnection() method to create a Connection object, which represents a physical connection with the database.
+		Execute a query: Requires using an object of type Statement for building and submitting an SQL statement to the database.
+		Extract data from result set: Requires that you use the appropriate ResultSet.getXXX() method to retrieve the data from the result set.
+		Clean up the environment: Requires explicitly closing all database resources versus relying on the JVM's garbage collection.
+	*/	
+	
+		Connection conn = null;
+		Statement stmt = null;
+		
+	try{	
+		// 2 단계 : Register JDBC driver
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		// 3 단계 : Open a connection 
+		conn = DriverManager.getConnection("jdbc:oracle:thin:@211.238.142.52:1521:xe", "rlawogns", "1111");
+		
+		// 4 단계 : 쿼리 준비 및 실행
+		// \r\n : 콘솔에서의 줄바꿈 처리
+		// 주의) 쿼리 끝 부분에 ';' 문자 없다 + 는  첫 번째 문장과  두 번째 문장을 서로 연결시켜준다.
+		String sql = "SELECT id_, age, first_, last_\r\n" + 
+				"    FROM employees";
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		// 5 단계 : 쿼리 실행 결과 분석 및 출력
+		
+		rs.next(); // 첫번째 row 부터 실행 // 마지막 row에서 실행시 flase 반환
+		int id_ = rs.getInt("id_"); 
+		int age = rs.getInt("age");
+		String first_ = rs.getString("first_");
+		String last_ = rs.getString("last_");
+	
+		System.out.printf("%d / %d / %s / %s%n",id_, age, first_, last_);
+	} catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+	} finally {
+		// 6 단계 : Clean-up environment
+		try{
+	         if(stmt!=null)
+	            stmt.close();
+	      } catch(SQLException se2){
+	      }
+		
+		try{
+	         if(conn!=null)
+	            conn.close();
+	      } catch(SQLException se){
+	         se.printStackTrace();
+	      }
+	}	
+		
+	}
+
+}
