c0cba7dc939b40fe951a37b464af91fdf87c9560
kjhoon1994 <47774697+kjhoon1994@users.noreply.github.com>
2019-05-02 17:09:16 +0900

    MySQL

--- 05.JSP/dba/20190502.sql
+++ 05.JSP/dba/20190502.sql
@@ -10,4 +10,22 @@ create table Employees
 insert INTO Employees(id, age, first, last) VALUES (100, 18, 'Zara','Ali');
 insert INTO Employees(id, age, first, last) VALUES (101, 16, 'Magnaz','Fatma');	
 
-SELECT * FROM Employees;
+SELECT id, age, first, last FROM Employees;
+
+SELECT INSTR('MySQL INSTR','SS') ;
+
+CREATE TABLE members
+	(
+		id int not null,
+        name_ varchar (50),
+        phone varchar (20),
+        email varchar (50)
+    );
+ALTER TABLE members 
+	ADD CONSTRAINT members_id_PK PRIMARY KEY(id);
+    
+INSERT INTO members(id, name_, phone, email) VALUES (100, 'hong', '010-1111-2222', 'hong@naver.com');		
+INSERT INTO members(id, name_, phone, email) VALUES (101, 'park', '010-1111-3333', 'park@naver.com');
+INSERT INTO members(id, name_, phone, email) VALUES (102, 'kim', '010-1111-4444', 'kim@naver.com');
+
+SELECT id, name_, phone, email FROM members;
