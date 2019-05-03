085213d040ec1d0cbbf8688a9bf9ffa65c8f5179
kjhoon1994 <47774697+kjhoon1994@users.noreply.github.com>
2019-05-03 12:24:17 +0900

    아

--- 05.JSP/dba/20190502.sql
+++ 05.JSP/dba/20190502.sql
@@ -1,11 +1,10 @@
-use TEST;
 create table Employees
-   (
-      id int not null,
-      age int not null,
-      first varchar (255),
-      last varchar (255)
-   );
+(
+	id int not null,
+    age int not null,
+	first varchar (255),
+	last varchar (255)
+);
 
 insert INTO Employees(id, age, first, last) VALUES (100, 18, 'Zara','Ali');
 insert INTO Employees(id, age, first, last) VALUES (101, 16, 'Magnaz','Fatma');	
@@ -27,5 +26,9 @@ ALTER TABLE members
 INSERT INTO members(id, name_, phone, email) VALUES ('M001', 'hong', '010-1111-2222', 'hong@naver.com');		
 INSERT INTO members(id, name_, phone, email) VALUES ('M002', 'park', '010-1111-3333', 'park@naver.com');
 INSERT INTO members(id, name_, phone, email) VALUES ('M003', 'kim', '010-1111-4444', 'kim@naver.com');
+commit;
 
 SELECT id, name_, phone, email FROM members;
+
+DELETE FROM members;
+SET SQL_SAFE_UPDATES = 0; /*0 삭제 가능하게 해준다 1 삭제 불가능하게 해준다 */
